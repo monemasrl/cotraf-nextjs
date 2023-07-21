fa0faf05be522555f1454140c39c2c52ed844c6c
grafica <paolo@monema.it>
2023-07-21 11:16:21 +0200

    form recaptcha

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="it">
       <head>
         <link rel="icon" href="/favicon.ico" />
       </head>
